8bb71674c690d6da00e11b0dad2972a6267afac9
MAubin <mazacra12345@gmail.com>
2023-01-23 22:24:10 -0500

    Render du board

---
+++ src/board.py
@@ -0,0 +1,44 @@
+from const import *
+from square import Square
+from piece import *
+
+class Board:
+    def __init__(self):
+        self.squares = [[0,0,0,0,0,0,0,0] for col in range(COLS)]
+
+        self._create()
+        self._add_pieces('white')
+        self._add_pieces('black')
+
+    def _create(self):
+        for row in range(ROW):
+            for col in range(COLS):
+                self.squares[row][col] = Square(row, col)
+    
+    def _add_pieces(self, color):
+        row_pawn, row_other = (6, 7) if color == 'white' else (1, 0)
+
+        #All pawns
+        for col in range(COLS):
+            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color))
+        
+        #All knights
+        self.squares[row_other][1] = Square(row_other, 1, Knight(color))
+        self.squares[row_other][6] = Square(row_other, 6, Knight(color))
+
+        #All Bishops
+        self.squares[row_other][2] = Square(row_other, 2, Bishop(color))
+        self.squares[row_other][5] = Square(row_other, 5, Bishop(color))
+
+        #All Rooks
+        self.squares[row_other][0] = Square(row_other, 0, Rook(color))
+        self.squares[row_other][7] = Square(row_other, 7, Rook(color))
+
+        #Queen
+        self.squares[row_other][3] = Square(row_other, 3, Queen(color))
+
+        #King
+        self.squares[row_other][4] = Square(row_other, 4, King(color))
+
+b = Board()
+b._create()
---
+++ src/const.py
@@ -0,0 +1,9 @@
+#Dimension de l'écran
+WIDTH = 800
+HEIGHT = 800
+
+#Dimension du jeu
+ROW = 8
+COLS = 8
+SQSIZE = WIDTH // COLS
+
